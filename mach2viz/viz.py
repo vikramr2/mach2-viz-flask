2699ab2f31e2064869b08177463ac66dcad2732b
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-11-10 12:56:40 -0600

    flexible port number

--- mach2viz/viz.py
+++ mach2viz/viz.py
@@ -4,6 +4,10 @@
 Server side for starting viz via commandline argument
 '''
 
+import fnmatch
+import os
+import re
+import socket
 import json
 import webbrowser
 import threading
@@ -19,12 +23,20 @@ class Viz:
         # Argument error handling
         if (not filename and not solution):
             raise TypeError('The visualizer requires either a filename or a solution object')
-        
+
         if (filename and solution):
-            raise TypeError('The visualizer requires either a filename or a solution object, not both.')
-        
+            raise TypeError(
+                'The visualizer requires either a filename or a solution object, not both.'
+            )
+
         # Setup the flask app by creating an instance of Flask
         self.app = Flask(__name__, static_url_path='/mach2-viz/')
+
+        # Set port to be the first free port
+        self.port = self._find_next_open_port(5000)
+
+        # Change client port number
+        self._change_port_in_client()
 
         # If a filename to a json is given, parse it and load data
         if filename:
@@ -59,14 +71,58 @@ class Viz:
     def run(self):
         ''' Open visualizer '''
         # Open the URL in the default web browser
-        url = f'http://127.0.0.1:5000/#/viz?labeling={self.labeling}&labeling2={self.labeling}'
+        url = f'http://127.0.0.1:{self.port}/#/viz?labeling={self.labeling}&labeling2={self.labeling}'
         threading.Timer(1, lambda: webbrowser.open(url)).start()
 
-        self.app.run()
+        self.app.run(port=self.port)
+
+    def _find_next_open_port(self, start_port):
+        current_port = start_port
+        while True:
+            try:
+                # Try to create a socket on the current port
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.bind(('localhost', current_port))
+                return current_port
+            except OSError:
+                # Port is not available, try the next one
+                current_port += 1
+
+    def _change_port_in_client(self):
+        ''' Goes through the client code and changes hardcoded port number '''
+        directory = 'static/static/js'
+        pattern = 'main*chunk.js*'
+
+        result = self._find_files(directory, pattern)
+
+        for file in result:
+            self._replace_port_number_in_file(file, self.port)
+
+    def _find_files(self, directory, pattern):
+        matches = []
+        for root, _, filenames in os.walk(directory):
+            for filename in fnmatch.filter(filenames, pattern):
+                matches.append(os.path.join(root, filename))
+        return matches
+
+    def _replace_port_number_in_file(self, file_path, new_port_number):
+        # Read the content of the file
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
+
+        # Define the regular expression pattern to match the substring
+        pattern = r"fetch\(['\"]http://127.0.0.1:(\d+)/json['\"]\)"
+
+        # Use re.sub to replace the matched port number with the new port number
+        replaced_content = re.sub(pattern, f"fetch('http://127.0.0.1:{new_port_number}/json')", file_content)
+
+        # Write the modified content back to the file
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(replaced_content)
 
 
 if __name__ == '__main__':  # If the script that was run is this script (we have not been imported)
     JSON_FILE = argv[1]
 
-    app = Viz(JSON_FILE)
+    app = Viz(filename=JSON_FILE)
     app.run()  # Start the server
